4cf574c0b06ef6b9ed3da837533d398bea160bfa
DvGuerraa <davimguerra@gmail.com>
2025-01-04 11:39:11 -0300

    Aula de condicional

--- index.php
+++ index.php
@@ -1,30 +1,21 @@
 <?php
-$Cidades = ["São Paulo", "Recife", "Bauneário Camboriú", "Rio de Janeiro", "Vila Velha"];
-echo $Cidades[2];
 
-$Alunos = [
-    'Davi' => [
-        'Idade' => 18
-    ],
-    'João' => [
-        'Idade' => 16
-    ],
-    'Maria' => [
-        'Idade' => 17
-    ]
-];
-var_dump($Alunos['João']['Idade']);
-
-$cores = ['azul', 'amarelo', 'verde'];
-$cores[] = 'vermelho';
-array_shift($cores);
-var_dump($cores);
-
-$preços = ["Lápis" => 3, "Caneta" => 5, "Borracha" => 4];
-$preços["Borracha"] = $preços["Borracha"] + 10;
-$preços["Lápis"] = $preços["Lápis"] - 1;
-print_r($preços);
+//Estruturas Condicionais
 
 
+// Operadores de Comparação
 
+// == (igual)
+// != (Diferente)
+
+// > (maior que)
+// < (menor que)
+//>= (maior oy igual a )
+// <= (menor ou igual a )
+$idade = 17;
+if ($idade >= 18){
+    echo "Você pode acessar o sistema";
+} else {
+    echo "Você NÃO pode acessar o sistema";
+}
 
