dd04ce22009432f43376938e29e0d00ac88e9660
DvGuerraa <davimguerra@gmail.com>
2025-01-04 12:38:07 -0300

    Operadores lógicos (Condição)

--- index.php
+++ index.php
@@ -1,21 +1,44 @@
 <?php
+// Operadores Lógicos
 
-//Estruturas Condicionais
+//&& Operador lógico E
+// Verifica se ambas condições são verdadeiras
 
+$idade = 18;
+$temCarteira = false;
 
-// Operadores de Comparação
-
-// == (igual)
-// != (Diferente)
-
-// > (maior que)
-// < (menor que)
-//>= (maior oy igual a )
-// <= (menor ou igual a )
-$idade = 17;
-if ($idade >= 18){
-    echo "Você pode acessar o sistema";
+if ($idade >= 18 && $temCarteira) {
+    echo 'Você pode dirigir no Brasil';
 } else {
-    echo "Você NÃO pode acessar o sistema";
+    echo 'Você NÃO pode dirigir no Brasil';
 }
 
+//|| Operador lógico OU
+$idade = 18;
+$temCarteira = false;
+if ($idade>=18 || $temCarteira == true){
+    echo 'Você pode dirigir uma nave espacial';
+} else {
+    echo "Você não pode dirigir uma nave espacil";
+}
+// ! Operador lógico de negação
+$temCarteira = 'ppd';
+
+// 0 == false
+// 1 == true
+//null = nulo, ou vazio
+
+if ($temCarteira === false) {
+    echo "Você não pode dirigir";
+} 
+
+elseif($temCarteira === true) {
+    echo "Você pode dirigir";
+}
+elseif($temCarteira === "ppd"){
+    echo "Você pode dirigir com cuidado";
+}
+
+else {
+    echo "Não sei se você pode dirigir";
+}
